aaca2f90bfd2fb70d76c9a4489caeefd98bb0fcb
Edward Gokmen <edward.gokmen@helsing.ai>
2023-02-22 21:18:32 +0000

    test 2 passes

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,18 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream};
+use quote::{quote};
 use syn::{
     parse_macro_input, DeriveInput
 };
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
+    let tokens = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let struct_name = tokens.ident;
+
+    (quote! {
+       impl #struct_name {
+            pub fn builder() {}
+        }
+    }).into()
 }
